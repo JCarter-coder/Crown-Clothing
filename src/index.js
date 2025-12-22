538ac9454d82b38b835586c2c11bd917b487487b
Jesse_Carter <jessetcarter@hotmail.com>
2025-12-22 14:24:52 -0700

    added netlify and stripe

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
 import { store, persistor } from './store/store';
-import reportWebVitals from './reportWebVitals';
+import { stripePromise } from './utils/stripe/stripe.utils';
+
+//import reportWebVitals from './reportWebVitals';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,7 +19,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
@@ -26,4 +31,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,15 +1,19 @@
 import { useSelector } from 'react-redux';
 
-import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
+import { 
+    selectCartItems, 
+    selectCartTotal 
+} from '../../store/cart/cart.selector';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import PaymentForm from '../../components/payment-form/payment-form.component';
+
 import {
     CheckoutContainer,
     CheckoutHeader,
     HeaderBlock,
     Total
 } from './checkout.styles';
-import { use } from 'react';
 
 const Checkout = () => {
     //const { cartItems, cartTotal } = useContext(CartContext);
@@ -39,6 +43,7 @@ const Checkout = () => {
                 <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
             ))}
             <Total>Total: ${cartTotal}</Total>
+            <PaymentForm />
         </CheckoutContainer>
     )
 };
